e928df04db6d798ef5e6b57a250589dae3b1ba52
dodelvin <139464046+dodelvin@users.noreply.github.com>
2026-04-28 19:35:54 +0400

    last working version

--- src/app/lib/stepCounter.ts
+++ src/app/lib/stepCounter.ts
@@ -4,9 +4,9 @@
  * on iOS for permission.
  */
 
-const STEP_THRESHOLD = 3;
-const MIN_STEP_INTERVAL_MS = 250;
-const MIN_HORIZONTAL_RATIO = 0.2;
+const STEP_THRESHOLD = 6;
+const MIN_STEP_INTERVAL_MS = 350;
+const MIN_HORIZONTAL_RATIO = 0.3;
 
 let started = false;
 let lastStepAt = 0;
